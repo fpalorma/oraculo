36c1c9530eaa2663f466fff3e129f0c2c8aebda9
Agustin Labiano <agustinlabiano@hotmail.com>
2024-08-03 22:01:14 +0000

    feat: add animation to rotate cards

--- src/app/components/TarotCardContainer.jsx
+++ src/app/components/TarotCardContainer.jsx
@@ -2,19 +2,15 @@
 import TarotCard from "./TarotCard";
 import Button from "./Button";
 import Input from "./Input";
-import CardBack from "./CardBack";
 import { useState } from "react";
-import { getCards } from "../lib/Cards";
+import { getCards, CARDS_BACK } from "../lib/Cards";
 import { getPrediction } from "../lib/ai-tarot";
 
 const TarotCardContainer = () => {
   const [cards, setCards] = useState([]);
   const [prediction, setPrediction] = useState(null);
-  const [isLoading, setIsLoading] = useState(false)
 
   const handleOnSubmit = async (formData) => {
-    
-    setIsLoading(false)
     const message = formData.get("userMessage");
     const tarotCards = getCards();
     const cardsNames = tarotCards.map((card) => card.name);
@@ -23,41 +19,37 @@ const TarotCardContainer = () => {
     setCards(response.isOk ? tarotCards : []);
   };
 
+  const onClick = () => {
+    setPrediction(null);
+    setCards(CARDS_BACK);
+  }
+
   return (
     <>
       <div>
-       
         <form
           className="flex flex-col gap-y-1 md:flex-row md:justify-center md:gap-x-2 items-center mb-4"
           action={handleOnSubmit}
         >
           <Input name="userMessage" />
-          <Button texto={"Consultar al oráculo"} handleOnClick={()=>setIsLoading(true)}/>
+          <Button texto={"Consultar al oráculo"} handleOnClick={onClick} />
         </form>
         {
-          !prediction &&  (
+          !prediction && (
             <p className="text-white text-center">
               Haz click en el botón para conocer tu suerte
             </p>
           )
         }
-      </div>
-    {
-      isLoading?<CardBack/>:(
-
+      </div>+
       <div className="flex  justify-center gap-x-4 mx-2 md:gap-x-12">
-        {cards.map((card) => (
-          <TarotCard key={card.id} card={card} />
-        ))}
+        {cards.map((card) => <TarotCard key={card.id} card={card} />)}
       </div>
-      )
-    }
-
-      {prediction&&!isLoading && (
-        <p className="text-fuchsia-500  text-pretty text-center mx-4 mt-2 pb-2 md:mx-60 md:mt-4 md:text-lg md:pb-10">
+      {prediction && (
+        <p className="animate__animated animate__fadeIn text-fuchsia-500 text-pretty text-center mx-4 mt-2 pb-2 md:mx-60 md:mt-4 md:text-lg md:pb-10">
           {prediction}
         </p>
-      ) }
+      )}
     </>
   );
 };
--- src/app/lib/Cards.js
+++ src/app/lib/Cards.js
@@ -1,149 +1,162 @@
 export const CARDS = [
-    {
-        id: "00",
-        name: "El Loco",
-        img: "/TAROT/r0.jpg",
-        prediction: "El Loco representa nuevos comienzos, aventuras y oportunidades. Esta carta simboliza la libertad, la espontaneidad y la fe en lo desconocido. Invita a confiar en el universo y a seguir la intuición sin miedo a lo que pueda venir. Aunque puede implicar riesgos, también sugiere que estos riesgos son necesarios para el crecimiento personal y la exploración de nuevos caminos."
-    },
-    {
-        id: "01",
-        name: "El Mago",
-        img: "/TAROT/r1.jpg",
-        prediction: "El Mago simboliza poder, habilidad y concentración. Representa la capacidad de manifestar tus deseos a través de la acción y la intención. Esta carta sugiere que tienes todas las herramientas necesarias para alcanzar tus metas, siempre que mantengas la concentración y utilices tus habilidades de manera efectiva."
-    },
-    {
-        id: "02",
-        name: "La Sacerdotisa",
-        img: "/TAROT/r2.jpg",
-        prediction: "La Sacerdotisa representa la sabiduría, la intuición y el conocimiento oculto. Esta carta te invita a confiar en tu intuición y buscar respuestas en tu interior. Es un llamado a la reflexión y la meditación, sugiriendo que las soluciones a tus preguntas se encuentran más allá del mundo material."
-    },
-    {
-        id: "03",
-        name: "La Emperatriz",
-        img: "/TAROT/r3.jpg",
-        prediction: "La Emperatriz simboliza la abundancia, la fertilidad y la creatividad. Representa el crecimiento, la prosperidad y la conexión con la naturaleza. Esta carta sugiere un período de crecimiento y realización en diversas áreas de tu vida, incluyendo relaciones, proyectos y creatividad."
-    },
-    {
-        id: "04",
-        name: "El Emperador",
-        img: "/TAROT/r4.jpg",
-        prediction: "El Emperador representa el poder, la autoridad y la estructura. Simboliza el control, la disciplina y la responsabilidad. Esta carta sugiere la necesidad de tomar el control de tu vida y establecer una base sólida para el futuro, utilizando la lógica y el razonamiento."
-    },
-    {
-        id: "05",
-        name: "El Hierofante",
-        img: "/TAROT/r5.jpg",
-        prediction: "El Hierofante representa la tradición, la religión y el conocimiento espiritual. Esta carta sugiere que es un buen momento para buscar guía espiritual o educativa y respetar las normas y convenciones establecidas. También puede indicar la necesidad de compartir tu conocimiento y enseñar a otros."
-    },
-    {
-        id: "06",
-        name: "Los Enamorados",
-        img: "/TAROT/r6.jpg",
-        prediction: "Los Enamorados simbolizan el amor, la unión y las decisiones. Esta carta representa las relaciones significativas y las decisiones importantes en tu vida. Sugiere la necesidad de tomar decisiones basadas en el amor y la armonía, y puede indicar una elección entre caminos diferentes."
-    },
-    {
-        id: "07",
-        name: "El Carro",
-        img: "/TAROT/r7.jpg",
-        prediction: "El Carro representa la determinación, la fuerza de voluntad y el triunfo. Simboliza el éxito alcanzado a través del esfuerzo y la perseverancia. Esta carta sugiere que tienes el control de tu destino y que el progreso y la victoria son posibles si mantienes el enfoque y la determinación."
-    },
-    {
-        id: "08",
-        name: "La Fuerza",
-        img: "/TAROT/r8.jpg",
-        prediction: "La Fuerza simboliza el coraje, la paciencia y el control interno. Representa la capacidad de superar los desafíos a través de la fuerza de carácter y la compasión. Esta carta sugiere que la verdadera fuerza proviene del equilibrio y la comprensión, no de la agresión o el dominio."
-    },
-    {
-        id: "09",
-        name: "El Ermitaño",
-        img: "/TAROT/r9.jpg",
-        prediction: "El Ermitaño representa la introspección, la soledad y la sabiduría interior. Simboliza la búsqueda de la verdad y el conocimiento a través de la reflexión y la meditación. Esta carta sugiere la necesidad de retirarse del mundo exterior para encontrar claridad y comprensión en tu camino espiritual."
-    },
-    {
-        id: "10",
-        name: "La Rueda de la Fortuna",
-        img: "/TAROT/r10.jpg",
-        prediction: "La Rueda de la Fortuna representa el cambio, el destino y los ciclos de la vida. Simboliza los altibajos y la naturaleza impredecible del destino. Esta carta sugiere que los cambios están en el horizonte y que es importante adaptarse y aceptar lo que venga con una actitud positiva."
-    },
-    {
-        id: "11",
-        name: "La Justicia",
-        img: "/TAROT/r11.jpg",
-        prediction: "La Justicia simboliza el equilibrio, la equidad y la verdad. Representa la necesidad de tomar decisiones justas y equilibradas. Esta carta sugiere que las acciones pasadas tendrán consecuencias y que es importante actuar con integridad y responsabilidad."
-    },
-    {
-        id: "12",
-        name: "El Colgado",
-        img: "/TAROT/r12.jpg",
-        prediction: "El Colgado representa el sacrificio, la rendición y una nueva perspectiva. Simboliza la necesidad de dejar ir y aceptar las circunstancias para encontrar claridad. Esta carta sugiere que el cambio de perspectiva y la paciencia son necesarios para avanzar."
-    },
-    {
-        id: "13",
-        name: "La Muerte",
-        img: "/TAROT/r13.jpg",
-        prediction: "La Muerte simboliza la transformación, el final de un ciclo y el renacimiento. Representa la necesidad de dejar ir lo viejo para dar paso a lo nuevo. Esta carta sugiere que, aunque los cambios pueden ser difíciles, son necesarios para el crecimiento y la evolución personal."
-    },
-    {
-        id: "14",
-        name: "La Templanza",
-        img: "/TAROT/r14.jpg",
-        prediction: "La Templanza representa el equilibrio, la moderación y la armonía. Simboliza la necesidad de encontrar un punto medio y combinar diferentes aspectos de la vida de manera equilibrada. Esta carta sugiere la importancia de la paciencia y la cooperación en la consecución de tus objetivos."
-    },
-    {
-        id: "15",
-        name: "El Diablo",
-        img: "/TAROT/r15.jpg",
-        prediction: "El Diablo simboliza la tentación, la adicción y la materialidad. Representa las ataduras y las ilusiones que nos impiden avanzar. Esta carta sugiere la necesidad de enfrentar y superar las influencias negativas y liberarse de las cadenas que te atan."
-    },
-    {
-        id: "16",
-        name: "La Torre",
-        img: "/TAROT/r16.jpg",
-        prediction: "La Torre representa la destrucción, la revelación y el cambio abrupto. Simboliza la caída de estructuras falsas y la necesidad de reconstruir sobre bases más sólidas. Esta carta sugiere que, aunque los cambios pueden ser traumáticos, también son necesarios para el crecimiento y la renovación."
-    },
-    {
-        id: "17",
-        name: "La Estrella",
-        img: "/TAROT/r17.jpg",
-        prediction: "La Estrella simboliza la esperanza, la inspiración y la renovación. Representa la guía y la luz en tiempos de oscuridad. Esta carta sugiere que hay esperanza y que los sueños son alcanzables. Invita a mantener la fe y la positividad en el camino hacia tus metas."
-    },
-    {
-        id: "18",
-        name: "La Luna",
-        img: "/TAROT/r18.jpg",
-        prediction: "La Luna representa la intuición, el subconsciente y las ilusiones. Simboliza los miedos y las inseguridades que pueden surgir en el camino. Esta carta sugiere la necesidad de confiar en la intuición y enfrentar los miedos para encontrar la claridad."
-    },
-    {
-        id: "19",
-        name: "El Sol",
-        img: "/TAROT/r19.jpg",
-        prediction: "El Sol simboliza la felicidad, el éxito y la vitalidad. Representa la claridad, el optimismo y la realización. Esta carta sugiere que la alegría y el éxito están en el horizonte y que es un momento de celebración y logro."
-    },
-    {
-        id: "20",
-        name: "El Juicio",
-        img: "/TAROT/r20.jpg",
-        prediction: "El Juicio representa la renovación, la evaluación y el despertar. Simboliza el llamado a una nueva vida y la necesidad de dejar atrás el pasado. Esta carta sugiere que es el momento de hacer un balance de tu vida y tomar decisiones importantes para tu futuro."
-    },
-    {
-        id: "21",
-        name: "El Mundo",
-        img: "/TAROT/r21.jpg",
-        prediction: "El Mundo simboliza la culminación, la realización y la integración. Representa el logro de metas y la finalización de ciclos. Esta carta sugiere que has alcanzado un punto de plenitud y que es momento de celebrar tus logros y mirar hacia nuevas posibilidades."
-    },
+  {
+    id: "00",
+    name: "El Loco",
+    img: "/TAROT/r0.jpg",
+    prediction: "El Loco representa nuevos comienzos, aventuras y oportunidades. Esta carta simboliza la libertad, la espontaneidad y la fe en lo desconocido. Invita a confiar en el universo y a seguir la intuición sin miedo a lo que pueda venir. Aunque puede implicar riesgos, también sugiere que estos riesgos son necesarios para el crecimiento personal y la exploración de nuevos caminos."
+  },
+  {
+    id: "01",
+    name: "El Mago",
+    img: "/TAROT/r1.jpg",
+    prediction: "El Mago simboliza poder, habilidad y concentración. Representa la capacidad de manifestar tus deseos a través de la acción y la intención. Esta carta sugiere que tienes todas las herramientas necesarias para alcanzar tus metas, siempre que mantengas la concentración y utilices tus habilidades de manera efectiva."
+  },
+  {
+    id: "02",
+    name: "La Sacerdotisa",
+    img: "/TAROT/r2.jpg",
+    prediction: "La Sacerdotisa representa la sabiduría, la intuición y el conocimiento oculto. Esta carta te invita a confiar en tu intuición y buscar respuestas en tu interior. Es un llamado a la reflexión y la meditación, sugiriendo que las soluciones a tus preguntas se encuentran más allá del mundo material."
+  },
+  {
+    id: "03",
+    name: "La Emperatriz",
+    img: "/TAROT/r3.jpg",
+    prediction: "La Emperatriz simboliza la abundancia, la fertilidad y la creatividad. Representa el crecimiento, la prosperidad y la conexión con la naturaleza. Esta carta sugiere un período de crecimiento y realización en diversas áreas de tu vida, incluyendo relaciones, proyectos y creatividad."
+  },
+  {
+    id: "04",
+    name: "El Emperador",
+    img: "/TAROT/r4.jpg",
+    prediction: "El Emperador representa el poder, la autoridad y la estructura. Simboliza el control, la disciplina y la responsabilidad. Esta carta sugiere la necesidad de tomar el control de tu vida y establecer una base sólida para el futuro, utilizando la lógica y el razonamiento."
+  },
+  {
+    id: "05",
+    name: "El Hierofante",
+    img: "/TAROT/r5.jpg",
+    prediction: "El Hierofante representa la tradición, la religión y el conocimiento espiritual. Esta carta sugiere que es un buen momento para buscar guía espiritual o educativa y respetar las normas y convenciones establecidas. También puede indicar la necesidad de compartir tu conocimiento y enseñar a otros."
+  },
+  {
+    id: "06",
+    name: "Los Enamorados",
+    img: "/TAROT/r6.jpg",
+    prediction: "Los Enamorados simbolizan el amor, la unión y las decisiones. Esta carta representa las relaciones significativas y las decisiones importantes en tu vida. Sugiere la necesidad de tomar decisiones basadas en el amor y la armonía, y puede indicar una elección entre caminos diferentes."
+  },
+  {
+    id: "07",
+    name: "El Carro",
+    img: "/TAROT/r7.jpg",
+    prediction: "El Carro representa la determinación, la fuerza de voluntad y el triunfo. Simboliza el éxito alcanzado a través del esfuerzo y la perseverancia. Esta carta sugiere que tienes el control de tu destino y que el progreso y la victoria son posibles si mantienes el enfoque y la determinación."
+  },
+  {
+    id: "08",
+    name: "La Fuerza",
+    img: "/TAROT/r8.jpg",
+    prediction: "La Fuerza simboliza el coraje, la paciencia y el control interno. Representa la capacidad de superar los desafíos a través de la fuerza de carácter y la compasión. Esta carta sugiere que la verdadera fuerza proviene del equilibrio y la comprensión, no de la agresión o el dominio."
+  },
+  {
+    id: "09",
+    name: "El Ermitaño",
+    img: "/TAROT/r9.jpg",
+    prediction: "El Ermitaño representa la introspección, la soledad y la sabiduría interior. Simboliza la búsqueda de la verdad y el conocimiento a través de la reflexión y la meditación. Esta carta sugiere la necesidad de retirarse del mundo exterior para encontrar claridad y comprensión en tu camino espiritual."
+  },
+  {
+    id: "10",
+    name: "La Rueda de la Fortuna",
+    img: "/TAROT/r10.jpg",
+    prediction: "La Rueda de la Fortuna representa el cambio, el destino y los ciclos de la vida. Simboliza los altibajos y la naturaleza impredecible del destino. Esta carta sugiere que los cambios están en el horizonte y que es importante adaptarse y aceptar lo que venga con una actitud positiva."
+  },
+  {
+    id: "11",
+    name: "La Justicia",
+    img: "/TAROT/r11.jpg",
+    prediction: "La Justicia simboliza el equilibrio, la equidad y la verdad. Representa la necesidad de tomar decisiones justas y equilibradas. Esta carta sugiere que las acciones pasadas tendrán consecuencias y que es importante actuar con integridad y responsabilidad."
+  },
+  {
+    id: "12",
+    name: "El Colgado",
+    img: "/TAROT/r12.jpg",
+    prediction: "El Colgado representa el sacrificio, la rendición y una nueva perspectiva. Simboliza la necesidad de dejar ir y aceptar las circunstancias para encontrar claridad. Esta carta sugiere que el cambio de perspectiva y la paciencia son necesarios para avanzar."
+  },
+  {
+    id: "13",
+    name: "La Muerte",
+    img: "/TAROT/r13.jpg",
+    prediction: "La Muerte simboliza la transformación, el final de un ciclo y el renacimiento. Representa la necesidad de dejar ir lo viejo para dar paso a lo nuevo. Esta carta sugiere que, aunque los cambios pueden ser difíciles, son necesarios para el crecimiento y la evolución personal."
+  },
+  {
+    id: "14",
+    name: "La Templanza",
+    img: "/TAROT/r14.jpg",
+    prediction: "La Templanza representa el equilibrio, la moderación y la armonía. Simboliza la necesidad de encontrar un punto medio y combinar diferentes aspectos de la vida de manera equilibrada. Esta carta sugiere la importancia de la paciencia y la cooperación en la consecución de tus objetivos."
+  },
+  {
+    id: "15",
+    name: "El Diablo",
+    img: "/TAROT/r15.jpg",
+    prediction: "El Diablo simboliza la tentación, la adicción y la materialidad. Representa las ataduras y las ilusiones que nos impiden avanzar. Esta carta sugiere la necesidad de enfrentar y superar las influencias negativas y liberarse de las cadenas que te atan."
+  },
+  {
+    id: "16",
+    name: "La Torre",
+    img: "/TAROT/r16.jpg",
+    prediction: "La Torre representa la destrucción, la revelación y el cambio abrupto. Simboliza la caída de estructuras falsas y la necesidad de reconstruir sobre bases más sólidas. Esta carta sugiere que, aunque los cambios pueden ser traumáticos, también son necesarios para el crecimiento y la renovación."
+  },
+  {
+    id: "17",
+    name: "La Estrella",
+    img: "/TAROT/r17.jpg",
+    prediction: "La Estrella simboliza la esperanza, la inspiración y la renovación. Representa la guía y la luz en tiempos de oscuridad. Esta carta sugiere que hay esperanza y que los sueños son alcanzables. Invita a mantener la fe y la positividad en el camino hacia tus metas."
+  },
+  {
+    id: "18",
+    name: "La Luna",
+    img: "/TAROT/r18.jpg",
+    prediction: "La Luna representa la intuición, el subconsciente y las ilusiones. Simboliza los miedos y las inseguridades que pueden surgir en el camino. Esta carta sugiere la necesidad de confiar en la intuición y enfrentar los miedos para encontrar la claridad."
+  },
+  {
+    id: "19",
+    name: "El Sol",
+    img: "/TAROT/r19.jpg",
+    prediction: "El Sol simboliza la felicidad, el éxito y la vitalidad. Representa la claridad, el optimismo y la realización. Esta carta sugiere que la alegría y el éxito están en el horizonte y que es un momento de celebración y logro."
+  },
+  {
+    id: "20",
+    name: "El Juicio",
+    img: "/TAROT/r20.jpg",
+    prediction: "El Juicio representa la renovación, la evaluación y el despertar. Simboliza el llamado a una nueva vida y la necesidad de dejar atrás el pasado. Esta carta sugiere que es el momento de hacer un balance de tu vida y tomar decisiones importantes para tu futuro."
+  },
+  {
+    id: "21",
+    name: "El Mundo",
+    img: "/TAROT/r21.jpg",
+    prediction: "El Mundo simboliza la culminación, la realización y la integración. Representa el logro de metas y la finalización de ciclos. Esta carta sugiere que has alcanzado un punto de plenitud y que es momento de celebrar tus logros y mirar hacia nuevas posibilidades."
+  },
+];
+
+export const CARDS_BACK = [
+  {
+    id: "0",
+    img: "/TAROT/Tarot-back.jpg",
+  },
+  {
+    id: "1",
+    img: "/TAROT/Tarot-back.jpg",
+  },
+  {
+    id: "2",
+    img: "/TAROT/Tarot-back.jpg",
+  }
 ];
 
 
+export const getCards = () => {
+  const cardArray = []
 
-export const getCards = ()=>{
-    const cardArray = []
-    
-    
-    while(cardArray.length<3){
-        const num = Math.floor(Math.random()*21);
-        if(!cardArray.find(card=>card.id === CARDS[num].id)){
-            cardArray.push(CARDS[num]) 
-        }
+  while (cardArray.length < 3) {
+    const num = Math.floor(Math.random() * 21);
+    if (!cardArray.find(card => card.id === CARDS[num].id)) {
+      cardArray.push(CARDS[num])
     }
-    return cardArray
+  }
+  return cardArray
 }
