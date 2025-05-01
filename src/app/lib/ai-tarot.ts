b3eb2ae85933b9c03bfa7def4a44d5ab2949424f
fpalorma <fedepalorma@gmail.com>
2025-05-01 11:32:21 -0300

    changed model to 1.5

--- src/app/lib/ai-tarot.ts
+++ src/app/lib/ai-tarot.ts
@@ -5,7 +5,7 @@ import { createGoogleGenerativeAI } from '@ai-sdk/google';
 const google = createGoogleGenerativeAI({
   // custom settings
   apiKey: process.env.AI_API_KEY || "",
-  baseURL: "https://generativelanguage.googleapis.com/v1",  
+  baseURL: "https://generativelanguage.googleapis.com/v1beta",  
 });
 
 if (!process.env.AI_API_KEY) {
@@ -23,17 +23,18 @@ export async function getPrediction(userMessage: string, tarotCards: string[]) {
     
     Si el mensaje del usuario no es una pregunta válida como por ejemplo saludos, mensajes sin sentido o en otros idiomas, deberás indicar que se vuelva a realizar la pregunta de manera amable. 
     
-    Necesito que me devuelvas una respuesta en formato JSON como acontinuacion:
+    Necesito que me devuelvas una respuesta en formato JSON como a continuacion:
   
     { "message": "tu respuesta", "isOk": true }
-  
+  No incluyas backticks en la respuesta. Solo el JSON crudo.
+  Respondé únicamente con un JSON válido, sin formatearlo como bloque de código, sin comillas triples ni backticks. Solo el objeto JSON.
 
     Es muy importante que tengas en cuenta que si el mensaje del usuario no es una pregunta válida como por ejemplo saludos, consultas sobre el clima, palabras sueltas en una oracion que no están formuladas como una pregunta directa(como "perro", "casa", "trabajo", "animal" o similares) y sin sentido, mensajes que incluyan una sola letra o un número solo, o mensajes sin sentido o en otros idiomas, deberás indicar que se vuelva a realizar la pregunta de manera amable e isOk es false.
     `
 
   
     const { text } = await generateText({
-      model: google('models/gemini-pro', { safetySettings: [
+      model: google('models/gemini-1.5-pro-latest', { safetySettings: [
         { category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE' },
         { category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE' },
         { category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE' },
